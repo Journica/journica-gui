31312dd0f498a761fc3c9a50272c7ff3a3231252
enticies <enticies@protonmail.com>
2026-01-16 07:04:23 +0400

    chore: Remove unused dependencies and clean up codebase

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,18 +1,18 @@
 use std::sync::Mutex;
 
-use crate::AppState;
+use crate::{AppState, AudioCommand};
 
 
 #[tauri::command]
 pub fn start_recording(state: tauri::State<Mutex<AppState>>) {
-    let mut state = state.lock().unwrap();
-    state.is_recording = true;
+    let state = state.lock().unwrap();
+    state.command_tx.send(AudioCommand::Start);
     println!("Start recording");
 }
 
 #[tauri::command]
 pub fn stop_recording(state: tauri::State<Mutex<AppState>>) {
-    let mut state = state.lock().unwrap();
-    state.is_recording = false;
+    let state = state.lock().unwrap();
+    state.command_tx.send(AudioCommand::Stop);
     println!("Stop recording");
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,40 @@
 mod commands;
-use std::sync::Mutex;
+use std::{sync::{Mutex, mpsc::{self, Sender}}, thread};
+
+pub enum AudioCommand {
+    Start, 
+    Stop
+}
 
 pub struct AppState {
-    is_recording: bool,
+    command_tx: Sender<AudioCommand>
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let (tx, rx) = mpsc::channel::<AudioCommand>();
+
+    thread::spawn(move || {
+        let mut stream: Option<cpal::Stream> = None;
+
+        match rx.recv() {
+            Ok(AudioCommand::Start) => {
+                println!("Start recording...");
+            },
+            Ok(AudioCommand::Stop) => {
+                println!("Stop recording...");
+                stream = None;
+            },
+            Err(_) => {
+                println!("Channel closed.");
+            }
+        }
+    });
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(Mutex::new(AppState {
-            is_recording: false
+            command_tx: tx
         }))
         .invoke_handler(tauri::generate_handler![commands::start_recording, commands::stop_recording])
         .run(tauri::generate_context!())
